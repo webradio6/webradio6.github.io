8afbc7ab9609410f034d8788d4c26236de4e16f3
webradio6 <130080505+webradio6@users.noreply.github.com>
2023-04-06 18:28:59 +0200

    Add files via upload

---
+++ WebRadio_entdecken_files/WebRadio_entdecken.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0 local-build-20230406
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,101),url:'WebRadio_entdecken_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'WebRadio_entdecken_files/stroke_1.png'},{rect:new IWRect(1,-1,318,2),url:'WebRadio_entdecken_files/stroke_2.png'},{rect:new IWRect(319,-1,3,2),url:'WebRadio_entdecken_files/stroke_3.png'},{rect:new IWRect(319,1,3,101),url:'WebRadio_entdecken_files/stroke_4.png'},{rect:new IWRect(319,102,3,2),url:'WebRadio_entdecken_files/stroke_5.png'},{rect:new IWRect(1,102,318,2),url:'WebRadio_entdecken_files/stroke_6.png'},{rect:new IWRect(-1,102,2,2),url:'WebRadio_entdecken_files/stroke_7.png'}],new IWSize(321,103)),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('WebRadio_entdecken_files/WebRadio_entdeckenMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
